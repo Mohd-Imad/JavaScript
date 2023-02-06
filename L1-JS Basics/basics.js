bd350e32dd0ee79d6469096efbd9754ea9133286
Mohd-Imad <mohdimd123@gmail.com>
2023-02-06 22:06:55 +0530

    ok

---
+++ L1-JS Basics/basics.js
@@ -0,0 +1,35 @@
+// JavaScript Variables and Data types
+
+
+//String
+let newStr = 'JavaScript'
+
+console.log(newStr);
+console.log(typeof(newStr));
+
+//Number
+let newNum = 44
+
+console.log(newNum);
+console.log(typeof(newNum));
+
+//Boolean
+let newBool = true
+
+console.log(newBool);
+console.log(typeof(newBool));
+
+newBool = newNum < 40
+
+console.log(newBool);
+console.log(typeof(newBool));
+
+
+//Undefined
+let myVar;
+
+console.log(myVar);
+console.log(typeof(myVar));
+
+
+
---
+++ L2-JS Linking to HTML/main.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+
+console.log(typeof('Imad'));
+console.log(typeof(44));
+console.log(typeof(true));
+console.log(typeof(newVar));
+
