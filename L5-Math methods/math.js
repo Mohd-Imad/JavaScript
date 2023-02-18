a2b323164ef831b58fd68dabb9e013e802508533
Mohd-Imad <mohdimd123@gmail.com>
2023-02-18 21:58:43 +0530

    ok

---
+++ L5-Math methods/math.js
@@ -0,0 +1,41 @@
+//Math methods
+console.log(Math.PI);   //returns pi value---3.141592653589793
+console.log(Math.trunc(Math.PI));   //returns pi value without any decimal points---3
+
+console.log(Math.round(Math.PI));   //returns nearest integer value---3
+console.log(Math.round(3.4));   //returns nearest integer value---3
+console.log(Math.round(3.5));   //returns nearest integer value---4
+
+console.log(Math.ceil(3.4));   //returns nearest increased integer value---4
+console.log(Math.floor(3.4));   //returns nearest decreased integer value---3
+
+console.log(Math.pow(2,10));   /* here 2-base value & 10-exponential value (2 to the power of 10) 
+                               and returns the base expression according to its power---1024 */
+
+console.log(Math.min(2,5,10));  //returns the minimum value of passed value---2
+console.log(Math.min(2,0.5,10));  //0.5
+console.log(Math.max(2,4,6,10));  //returns the maximum value of passed value---10
+console.log(Math.max(2,4,6,6.3));  //6.3
+
+console.log(Math.random());  //returns a float random number from 0 to 1
+
+//Random number from 1 to 10
+console.log(Math.floor(Math.random()*10)+1); //returns an integer random number from 1-10
+console.log(Math.floor(Math.random()*10)+1); //If we use Math.ceil() instead of Math.floor() this has a chance of returning 0
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+
+
+
+
+
+
+
