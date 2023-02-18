42f863ae437d52125758747be8ad12f648656e40
Mohd-Imad <mohdimd123@gmail.com>
2023-02-18 16:29:17 +0530

    ok

--- L4-JS Numbers/numbers.js
+++ L4-JS Numbers/numbers.js
@@ -5,10 +5,10 @@ let myNumber = 18
 console.log(myNumber);
 
 //A number with a decimal point is a float which references the 'floating point'
-let myFloat = 18.0
+let myFloat = 18.01
 console.log(myFloat);
 
-let myString = '18'
+let myString = '18.0123vfvfd'
 console.log(myString);
 
 //Comparing 
@@ -24,5 +24,24 @@ console.log(typeof(Number(myString)));  //number
 console.log(Number(myString) + 3);  //here addition ll be performed, coz now string converted to number
 
 console.log(Number(myString) === myNumber); //true
+
+//Number methods
+console.log(Number.isInteger(myNumber));   //true --checks the number is integer or not
+console.log(Number.isInteger(myString));   //false
+
+console.log(Number.parseFloat(myString));  //returns float---while parsing this automatically removes non numeric chars anad change it's datatype to number DT
+
+console.log(Number.parseInt(myString));  //returns integer---while parsing this automatically removes non numeric chars anad change it's datatype to number DT
+
+console.log(Number.parseFloat(myString).toFixed(2));  //formats a number according to how many decimal points you provide as parameter
+
+console.log(myFloat.toString());  //returns a string representing the number
+console.log(typeof myFloat.toString());
+
+//NaN- Not a Number
+console.log(Number('Imad'));
+console.log(Number.isNaN('Imad')); //false --determines the value is NaN and its type is number
+console.log(isNaN('Imad')); //true  --determines the value is NaN or not
+
 
 
