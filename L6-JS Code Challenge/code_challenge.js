87a930107a5a3713db95d316cf7cc80579a7738f
Mohd-Imad <mohdimd123@gmail.com>
2023-02-18 22:15:51 +0530

    ok

---
+++ L6-JS Code Challenge/code_challenge.js
@@ -0,0 +1,25 @@
+//Code Challenge
+
+// WAP to return a random letter from your name
+
+let myName = 'Mohammed Imad'
+
+//step-1 ---random number from 1-12
+console.log(Math.floor(Math.random()*12)+1); 
+
+//step-2 ---return particular string value based on index value passed 
+console.log(myName.charAt(3));  
+
+//step-3 ---returns a random letter from the given string
+console.log(myName.charAt(Math.floor(Math.random()*12)+1)); 
+console.log(myName.charAt(Math.floor(Math.random()*12)+1));
+console.log(myName.charAt(Math.floor(Math.random()*12)+1));
+console.log(myName.charAt(Math.floor(Math.random()*12)+1));
+console.log(myName.charAt(Math.floor(Math.random()*12)+1));
+console.log(myName.charAt(Math.floor(Math.random()*12)+1));
+console.log(myName.charAt(Math.floor(Math.random()*12)+1));
+console.log(myName.charAt(Math.floor(Math.random()*12)+1));
+console.log(myName.charAt(Math.floor(Math.random()*12)+1));
+console.log(myName.charAt(Math.floor(Math.random()*12)+1));
+console.log(myName.charAt(Math.floor(Math.random()*12)+1));
+console.log(myName.charAt(Math.floor(Math.random()*12)+1));
